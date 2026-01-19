20ea6cad3806f6a06f4263578fe1b2d8a504c343
gisuk <gisuk0222@gmail.com>
2026-01-19 10:44:47 +0900

    env add

--- app.py
+++ app.py
@@ -10,7 +10,16 @@ import pandas as pd
 import FinanceDataReader as fdr
 import matplotlib.pyplot as plt
 import koreanize_matplotlib
+import os
 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+my_name = os.getenv('MY_NAME')
+st.header(my_name)
 def get_krx_company_list() -> pd.DataFrame:
     try:
         # 파이썬 및 인터넷의 기본 문자열 인코딩 방식- UTF-8
